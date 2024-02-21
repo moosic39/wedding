e86b4c9d0056da6d8a0a914708f5fd7a9cbc2e02
Mickael JEGAT <moosic39@gmail.com>
2024-02-21 09:21:07 +0100

    feat: update hero with letter month

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { IconButton, Button, Typography } from '@material-tailwind/react'
-import { PlayIcon } from '@heroicons/react/24/solid'
+import { Typography } from '@material-tailwind/react'
 
 function Hero() {
   return (
@@ -11,7 +10,6 @@ function Hero() {
         <div className='container relative z-10 my-auto mx-auto grid place-items-center text-center'>
           <Typography
             variant='h2'
-            color='cyan'
             className='mb-2 text-white text-2xl md:text-3xl lg:text-4xl'
             placeholder={undefined}
           >
@@ -29,19 +27,8 @@ function Hero() {
             className='mt-1 mb-12 w-full md:max-w-full text-xl md:text-2xl lg:text-3xl text-white'
             placeholder={undefined}
           >
-            21.09.2024
+            ﹣ 21 Septembre 2024 ﹣
           </Typography>
-          {/* <div className='flex items-center gap-4'>
-            <Button variant='gradient' color='white' placeholder={undefined}>
-              Get started
-            </Button>
-            <IconButton
-              className='rounded-full bg-white p-6'
-              placeholder={undefined}
-            >
-              <PlayIcon className='h-4 w-4 text-gray-900' />
-            </IconButton>
-          </div> */}
         </div>
       </div>
     </div>
