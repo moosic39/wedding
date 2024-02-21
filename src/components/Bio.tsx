ccf478e8ec34491a86267e251b1e36b73ae8054e
Mickael JEGAT <mickael@Air-de-Famille.lan>
2024-02-21 02:47:32 +0100

    feat: add bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,47 +1,47 @@
 'use client'
 import React from 'react'
 
-import Card from './ui-components/atom/Card'
+import Card from './ui-components/atom/ProfileCard'
 import { Container, Title } from './ui-components/atom'
 
 const Bio = () => {
   const jennProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Jennifer.webp',
     title: 'Jennifer Da Silva',
     desc: 'La Mariée',
   }
   const mickaProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Mickael.webp',
     title: 'Mickaël Jégat',
     desc: 'Le Marié',
   }
   const aurelProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Aurelie.webp',
     title: 'Aurélie Da Silva',
     desc: 'Sœur de la mariée',
   }
   const ingridProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Ingrid.webp',
     title: 'Ingrid Ramon',
-    desc: 'Amie de la mariée',
+    desc: 'Meilleure amie de la mariée',
   }
   const auroreProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Aurore.webp',
     title: 'Aurore Gaumel',
     desc: 'Amie de la mariée',
   }
   const benoitProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Benoit.webp',
     title: 'Benoit Boudier',
     desc: 'Frère de ❤ du marié',
   }
   const jeromeProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Jerome.webp',
     title: 'Jérôme Perrard',
     desc: 'Binôme du marié',
   }
   const alexyProps = {
-    src: 'https://tecdn.b-cdn.net/img/new/avatars/5.webp',
+    src: '/image/Alexy.webp',
     title: 'Alexy Guillon',
     desc: 'Meilleur ami du marié',
   }
@@ -51,12 +51,12 @@ const Bio = () => {
       title='Les Mariées'
       className='container mx-auto flex flex-col items-center gap-4 px-4 py-10'
     >
-      <div className='md:flex flex-row w-full justify-between gap-8'>
+      <div className='md:flex flex-row w-full justify-between gap-8 mb-12'>
         <Card {...jennProps} />
         <Card {...mickaProps} />
       </div>
       <Title title={"Les demoiselles d'honneur"} />
-      <div className='lg:flex flex-row gap-4 w-full'>
+      <div className='lg:flex flex-row gap-4 w-full mb-4'>
         <Card {...aurelProps} />
         <Card {...ingridProps} />
         <Card {...auroreProps} />
@@ -64,8 +64,8 @@ const Bio = () => {
       <Title title={"Les garçons d'honneur"} />
       <div className='lg:flex flex-row gap-4 w-full'>
         <Card {...benoitProps} />
-        <Card {...alexyProps} />
         <Card {...jeromeProps} />
+        <Card {...alexyProps} />
       </div>
     </Container>
   )
