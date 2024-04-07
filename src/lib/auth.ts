8ecf2eff78ef437d210bbf6bca5dff0dd3316237
Mickael JEGAT <moosic39@gmail.com>
2024-04-08 00:03:17 +0200

    feat: add google oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,11 @@
 import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/lib/prisma'
 import NextAuth from 'next-auth'
 
 const authOptions = {
-  providers: [GitHub],
+  providers: [GitHub, Google],
   adapter: PrismaAdapter(prisma),
 }
 
