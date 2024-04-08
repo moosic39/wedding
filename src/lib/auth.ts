7e280a656523abd9a8cc8801792f59611d6b2e8e
Mickael JEGAT <moosic39@gmail.com>
2024-04-08 02:02:28 +0200

    feat: add nodemailer login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,26 @@
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
+import Email from 'next-auth/providers/nodemailer'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/lib/prisma'
 import NextAuth from 'next-auth'
 
 const authOptions = {
-  providers: [GitHub, Google],
+  providers: [
+    Email({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    Google,
+    GitHub,
+  ],
   adapter: PrismaAdapter(prisma),
 }
 
