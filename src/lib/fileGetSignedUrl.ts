907677a57c282793170337e2643c9c5f17de3a6f
Mickael JEGAT <moosic39@gmail.com>
2024-04-21 14:34:51 +0200

    feat: get signedUrl from file

--- src/lib/fileGetSignedUrl.ts
+++ src/lib/fileGetSignedUrl.ts
@@ -1,18 +1,26 @@
-import { getSignedUrl, S3RequestPresigner } from '@aws-sdk/s3-request-presigner'
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+'use server'
+
 import { Bucket, s3Config } from '@/constant/s3Config'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 
-const client = new S3Client({ ...s3Config })
-const command = new GetObjectCommand(getObjectParams)
-const url = getSignedUrl(client, command)
-
-import { Hash } from '@aws-sdk/hash-node'
-const signer = new S3RequestPresigner({
-  region: regionProvider,
-  credentials: credentialsProvider,
-  sha256: Hash.bind(null, 'sha256'), // In Node.js
-  //sha256: Sha256 // In browsers
+const s3 = new S3Client({
+  ...s3Config,
 })
-const presigned = await signer.presign(request)
 
-// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-s3-request-presigner/
+export const getPreSignedImage = async (
+  formData: FormData,
+): Promise<string> => {
+  const fileName = formData.get('fileName') as string
+  const folderName = formData.get('folderName') as string
+  const getCommand = new GetObjectCommand({
+    Bucket,
+    Key: `${folderName}/${fileName}`,
+  })
+  try {
+    return await getSignedUrl(s3, getCommand, { expiresIn: 3600 })
+  } catch (e) {
+    console.error(e)
+    return 'Error: Image Upload failed'
+  }
+}
