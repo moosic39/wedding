3f85aa4da6f991299bd37d6f064b945c49ef9be3
Mickael JEGAT <moosic39@gmail.com>
2024-04-21 14:32:26 +0200

    feat: add helper to stringify

--- src/helpers/stringParser.ts
+++ src/helpers/stringParser.ts
@@ -1,8 +1,22 @@
 export const snakeCase = (str: string) => {
   return str
-    .replace(/([A-Z])/g, ' $1')
-    .replace(/^./, (str) => str.toLowerCase())
-    .replace(/ /g, '_')
+    .replace(/\s+/g, '_')
+    .toLowerCase()
+    .replace(/[^a-z0-9_]/g, '')
+    .replace(/_{2,}/g, '_')
+    .replace(/^_/, '')
+    .replace(/_$/, '')
+}
+
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export const capitalizeSnakeCase = (str: string) => {
+  return str
+    .split(' ')
+    .map((word) => capitalize(word))
+    .join('_')
 }
 
 export const camelCase = (str: string) => {
