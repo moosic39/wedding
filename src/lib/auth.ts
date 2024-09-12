db85e45ac9a8c9e58aeb55163e06563139c7815a
Mickael JEGAT <moosic39@gmail.com>
2024-09-12 23:57:05 +0200

    feat: add twitter oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import Facebook from 'next-auth/providers/facebook'
 import GitHub from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 import Instagram from 'next-auth/providers/instagram'
+import Twitter from 'next-auth/providers/twitter'
 import Email from 'next-auth/providers/nodemailer'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/lib/prisma'
@@ -10,55 +11,56 @@ import { Provider } from '@auth/core/providers'
 import Credentials from '@auth/core/providers/credentials'
 
 const providers: Provider[] = [
-  // Credentials({
-  //   credentials: {
-  //     username: { label: 'Username' },
-  //     password: { label: 'Password', type: 'password' },
-  //   },
-  //   async authorize({ request }) {
-  //     const response = await fetch(request)
-  //     if (!response.ok) return null
-  //     return (await response.json()) ?? null
-  //   },
-  // }),
-  Email({
-    server: {
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    },
-    from: process.env.EMAIL_FROM,
-  }),
-  Google,
-  Facebook,
-  // Instagram,
-  GitHub,
+	// Credentials({
+	//   credentials: {
+	//     username: { label: 'Username' },
+	//     password: { label: 'Password', type: 'password' },
+	//   },
+	//   async authorize({ request }) {
+	//     const response = await fetch(request)
+	//     if (!response.ok) return null
+	//     return (await response.json()) ?? null
+	//   },
+	// }),
+	Email({
+		server: {
+			host: process.env.SMTP_HOST,
+			port: Number(process.env.SMTP_PORT),
+			auth: {
+				user: process.env.SMTP_USER,
+				pass: process.env.SMTP_PASSWORD,
+			},
+		},
+		from: process.env.EMAIL_FROM,
+	}),
+	Google,
+	Facebook,
+	Instagram,
+	GitHub,
+	Twitter,
 ]
 
 const authOptions: NextAuthConfig = {
-  providers,
-  adapter: PrismaAdapter(prisma),
-  // pages: {
-  //   signIn: '/signin',
-  //   newUser: '/signup',
-  // },
-  theme: {
-    colorScheme: 'dark',
-    brandColor: '#0096a7',
-    logo: '/logos/wedding_logo_v1_rounded.png',
-  },
+	providers,
+	adapter: PrismaAdapter(prisma),
+	// pages: {
+	//   signIn: '/signin',
+	//   newUser: '/signup',
+	// },
+	theme: {
+		colorScheme: 'dark',
+		brandColor: '#0096a7',
+		logo: '/logos/wedding_logo_v1_rounded.png',
+	},
 }
 
 export const providerMap = providers.map((provider) => {
-  if (typeof provider === 'function') {
-    const providerData = provider()
-    return { id: providerData.id, name: providerData.name }
-  } else {
-    return { id: provider.id, name: provider.name }
-  }
+	if (typeof provider === 'function') {
+		const providerData = provider()
+		return { id: providerData.id, name: providerData.name }
+	} else {
+		return { id: provider.id, name: provider.name }
+	}
 })
 
 export const { auth, handlers, signIn, signOut } = NextAuth(authOptions)
