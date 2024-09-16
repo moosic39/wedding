c79b395a3b250ee4147ae35c43f0c6c0d9fc4574
Mickael JEGAT <mickael@Air-de-Famille.lan>
2024-09-16 14:18:54 +0200

    fix: shuffle photos

--- src/app/slideshow/page.tsx
+++ src/app/slideshow/page.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { GalleryWithCarousel } from '@/components/ui-components/GalleryContainer'
 
 import { getAllMetadata } from '@/lib/getMetadata'
-import { random } from 'lodash'
 
 const SlideShow = async () => {
   const photos = await getAllMetadata()
-  const shufflePhotos = photos.toSorted(() => random(-1, 1))
+  photos.sort(() => Math.random() - 0.5)
 
   return (
     <div className='h-full w-full bg-blue-gray-900'>
-      <GalleryWithCarousel photos={shufflePhotos} />
+      <GalleryWithCarousel photos={photos} />
     </div>
   )
 }
